acb5a0dee9208d9cdccc89669bc5b6b2559fb53e
andrebriggs@users.noreply.github.com <andrebriggs@users.noreply.github.com>
2019-01-20 14:08:03 -0800

    Updated API

--- server.go
+++ server.go
@@ -6,11 +6,28 @@ import (
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:]) //substring after '/' character
+const version string = "1.0"
+
+// Old catch all handler
+// func handler(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:]) //substring after '/' character
+// }
+
+func getFrontpage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Congratulations! Version %s of your application is running on Kubernetes.", version)
+}
+
+func health(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
+
+func getVersion(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "%s\n", version)
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", getFrontpage)
+	http.HandleFunc("/health", health)
+	http.HandleFunc("/version", getVersion)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
