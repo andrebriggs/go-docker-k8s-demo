6546a47ad2840d7df04bbec5d2e9915c6fc7153b
andrebriggs@users.noreply.github.com <andrebriggs@users.noreply.github.com>
2019-03-19 23:54:41 -0700

    Updates to api outputs

--- server.go
+++ server.go
@@ -3,18 +3,30 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"time"
 )
 
-const version string = "1.2"
+var appVersion = "1.2" //Default/fallback version
+var instanceNum int
 
 // Old catch all handler
 // func handler(w http.ResponseWriter, r *http.Request) {
 // 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:]) //substring after '/' character
 // }
 
+// func getenv(key, fallback string) string {
+// 	value := os.Getenv(key)
+// 	if len(value) == 0 {
+// 		return fallback
+// 	}
+// 	return value
+// }
+
 func getFrontpage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Congratulations! Version %s of your application is running on Kubernetes.", version)
+	t := time.Now().UTC()
+	fmt.Fprintf(w, "Hi! I'm instance %d running version %s of your application at %s", instanceNum, appVersion, t.Format("2006-01-02 15:04:05"))
 }
 
 func health(w http.ResponseWriter, r *http.Request) {
@@ -22,10 +34,12 @@ func health(w http.ResponseWriter, r *http.Request) {
 }
 
 func getVersion(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "%s\n", version)
+	fmt.Fprintf(w, "%s\n", appVersion)
 }
 
 func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+	instanceNum = rand.Intn(1000)
 	http.HandleFunc("/", getFrontpage)
 	http.HandleFunc("/health", health)
 	http.HandleFunc("/version", getVersion)
